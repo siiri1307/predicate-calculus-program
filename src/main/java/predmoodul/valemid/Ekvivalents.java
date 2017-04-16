5dacfa51dee9bd80b654bb28a17b9106c131d14c
Siiri Saar <siiri.saar1307@gmail.com>
2017-04-16 22:44:25 +0300

    Truth tree rule method added, tree structure changed to containing two child nodes

--- src/main/java/predmoodul/valemid/Ekvivalents.java
+++ src/main/java/predmoodul/valemid/Ekvivalents.java
@@ -9,50 +9,111 @@ import java.util.*;
  */
 public class Ekvivalents extends Valem {
 
-    private List<Valem> implikatsiooniValemid;
+    private Valem vasakLaps;
+    private Valem paremLaps;
 
-    public Ekvivalents(List<Valem> impl){
-        this.implikatsiooniValemid = impl;
+    public Ekvivalents(Valem valem1, Valem valem2){
+
+        this.vasakLaps = valem1;
+        this.paremLaps = valem2;
     }
 
     @Override
     public List<Object> getChildren() {
-        return Arrays.asList((Object)implikatsiooniValemid);
+        return Arrays.asList((Object) vasakLaps, paremLaps);
     }
 
     @Override
     public boolean vaartusta(Map<Character, Double> vaartustus) {
 
-        return  implikatsiooniValemid.stream()
-                .map(valem -> valem.vaartusta(vaartustus))
-                .reduce((valem1,valem2) -> implikatsioon(valem1, valem2)).get();
+        return ekvivalents(vasakLaps.vaartusta(vaartustus), paremLaps.vaartusta(vaartustus));
+        
     }
 
-    private boolean implikatsioon(boolean valem1, boolean valem2) {
-        if(valem1 && !valem2){
-            return false;
+    private boolean ekvivalents(boolean valem1, boolean valem2) {
+        if((valem1 && valem2) || (!valem1 && !valem2)){
+            return true;
         }
-        return true;
+        return false;
     }
 
     @Override
     public Set<IndiviidTerm> getIndiviidTermid() {
+
         Set<IndiviidTerm> indiviidTermid = new HashSet<>();
 
-        for(Valem valem : implikatsiooniValemid){
-            indiviidTermid.addAll(valem.getIndiviidTermid());
-        }
+        indiviidTermid.addAll(paremLaps.getIndiviidTermid());
+        indiviidTermid.addAll(vasakLaps.getIndiviidTermid());
+
         return indiviidTermid;
     }
 
     public Set<Character> getVabadMuutujad() {
+
         Set<Character> vabad = new HashSet<>();
-        for(Valem valem : implikatsiooniValemid){
-            Set<Character> vabadMuutujad = valem.getVabadMuutujad();
-            if (vabadMuutujad != null) {
-                vabad.addAll(vabadMuutujad);
-            }
+
+        Set<Character> vabadMuutujadVasakul = vasakLaps.getVabadMuutujad();
+        Set<Character> vabadMuutujadParemal = paremLaps.getVabadMuutujad();
+
+        if(vabadMuutujadVasakul != null){
+            vabad.addAll(vabadMuutujadVasakul);
         }
+        if(vabadMuutujadParemal != null){
+            vabad.addAll(vabadMuutujadParemal);
+        }
+
         return vabad;
+    }
+
+    public static Valem looEkvivalentsid(List<Valem> alamValemid) {
+
+        //return alamValemid.stream().reduce((x,y) -> new Ekvivalents(x,y)).orElse(alamValemid.get(0));
+
+
+        Valem vasak = alamValemid.get(0);
+
+        for(int i = 1; i < alamValemid.size(); i++){
+            vasak = new Ekvivalents(vasak, alamValemid.get(i));
+
+        }
+
+        return vasak;
+    }
+
+    public List<TõesuspuuTipp> reegel(boolean tõeväärtus) {
+
+        TõesuspuuTipp vasakLaps;
+        TõesuspuuTipp lapseLapsVasakul;
+        TõesuspuuTipp paremLaps;
+        TõesuspuuTipp lapseLapsParemal;
+
+        if(tõeväärtus){
+
+            vasakLaps = new TõesuspuuTipp(this.vasakLaps, true);
+            lapseLapsVasakul = new TõesuspuuTipp(this.paremLaps, true);
+
+            vasakLaps.setVasakLaps(lapseLapsVasakul); //kui hargnemist ei toimu, siis laps vasakule
+
+            paremLaps = new TõesuspuuTipp(this.vasakLaps, false);
+            lapseLapsParemal = new TõesuspuuTipp(this.paremLaps, false);
+
+            paremLaps.setVasakLaps(lapseLapsParemal);
+
+            return Arrays.asList(vasakLaps, paremLaps);
+        }
+        else{
+
+            vasakLaps = new TõesuspuuTipp(this.vasakLaps, true);
+            lapseLapsVasakul = new TõesuspuuTipp(this.paremLaps, false);
+
+            vasakLaps.setVasakLaps(lapseLapsVasakul);
+
+            paremLaps = new TõesuspuuTipp(this.vasakLaps, false);
+            lapseLapsParemal = new TõesuspuuTipp(this.paremLaps, true);
+
+            paremLaps.setVasakLaps(lapseLapsParemal);
+
+            return Arrays.asList(vasakLaps, paremLaps);
+        }
     }
 }
