de12b8a055acb5368d1499fe2185a17c961866e5
Siiri Saar <siiri.saar1307@gmail.com>
2017-04-16 22:37:06 +0300

    New class to preserve the order of negation and quantifiers upon parsing

---
+++ src/main/java/predmoodul/kvantorid/ModifierEks.java
@@ -0,0 +1,13 @@
+package predmoodul.kvantorid;
+
+/**
+ * Created by siiri on 14/04/17.
+ */
+public class ModifierEks implements Modifier {
+
+    public Character indiviidmuutuja;
+
+    public ModifierEks(Character indiviidmuutuja) {
+        this.indiviidmuutuja = indiviidmuutuja;
+    }
+}
