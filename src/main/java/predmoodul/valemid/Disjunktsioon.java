6f767f59fefa6e95123ec467a7e6f17ee0e2f491
Siiri Saar <siiri.saar1307@gmail.com>
2017-04-16 22:43:17 +0300

    Truth tree rule method added, tree structure changed to containing two child nodes

--- src/main/java/predmoodul/valemid/Disjunktsioon.java
+++ src/main/java/predmoodul/valemid/Disjunktsioon.java
@@ -9,41 +9,73 @@ import java.util.*;
  */
 public class Disjunktsioon extends Valem{
 
-    private List<Valem> konjuktsiooniValemid;
+    private final Valem vasakLaps;
+    private final Valem paremLaps;
 
-    public Disjunktsioon(List<Valem> konj){
-        this.konjuktsiooniValemid = konj;
+    public Disjunktsioon(Valem valem1, Valem valem2){
+        this.vasakLaps = valem1;
+        this.paremLaps = valem2;
     }
 
     @Override
     public List<Object> getChildren() {
-        return Arrays.asList((Object) konjuktsiooniValemid);
+        return Arrays.asList((Object) vasakLaps, paremLaps);
     }
 
     @Override
     public boolean vaartusta(Map<Character, Double> vaartustus) {
 
-        return  konjuktsiooniValemid.stream()
-                .map(valem -> valem.vaartusta(vaartustus))
-                .reduce((valem1,valem2) -> valem1 && valem2).get();
+        return vasakLaps.vaartusta(vaartustus) || paremLaps.vaartusta(vaartustus);
+        
     }
 
     @Override
     public Set<IndiviidTerm> getIndiviidTermid() {
+
         Set<IndiviidTerm> indiviidTermid = new HashSet<>();
 
-        for(Valem valem : konjuktsiooniValemid){
-            indiviidTermid.addAll(valem.getIndiviidTermid());
-        }
+        indiviidTermid.addAll(vasakLaps.getIndiviidTermid());
+        indiviidTermid.addAll(paremLaps.getIndiviidTermid());
+
         return indiviidTermid;
     }
 
     @Override
     public Set<Character> getVabadMuutujad() {
+
         Set<Character> vabad = new HashSet<>();
-        for(Valem valem : konjuktsiooniValemid){
-            vabad.addAll(valem.getVabadMuutujad());
-        }
+
+        vabad.addAll(vasakLaps.getVabadMuutujad());
+        vabad.addAll(paremLaps.getVabadMuutujad());
+
         return vabad;
+    }
+
+    public static Valem looDisjunktsioonid(List<Valem> alamValemid) {
+
+        Valem vasak = alamValemid.get(0);
+
+        for(int i = 1; i < alamValemid.size(); i++){
+            vasak = new Disjunktsioon(vasak, alamValemid.get(i));
+
+        }
+
+        return vasak;
+    }
+
+    public List<TõesuspuuTipp> reegel(boolean tõeväärtus) {
+
+        if(tõeväärtus){
+            return Arrays.asList(new TõesuspuuTipp(this.vasakLaps, true), new TõesuspuuTipp(this.paremLaps, true));
+        }
+        else{
+
+            TõesuspuuTipp laps = new TõesuspuuTipp(vasakLaps, false);
+            TõesuspuuTipp lapseLaps = new TõesuspuuTipp(paremLaps, false);
+
+            laps.setVasakLaps(lapseLaps);
+
+            return Arrays.asList(laps);
+        }
     }
 }
