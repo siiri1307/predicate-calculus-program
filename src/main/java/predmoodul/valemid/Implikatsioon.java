78e921852906ad1d3785f1d134464aa5db8b4694
Siiri Saar <siiri.saar1307@gmail.com>
2017-04-16 22:44:53 +0300

    Truth tree rule method added, tree structure changed to containing two child nodes

--- src/main/java/predmoodul/valemid/Implikatsioon.java
+++ src/main/java/predmoodul/valemid/Implikatsioon.java
@@ -9,40 +9,99 @@ import java.util.*;
  */
 public class Implikatsioon extends Valem {
 
-    private List<Valem> disjunktsiooniValemid;
+    private Valem vasakLaps;
+    private Valem paremLaps;
 
-    public Implikatsioon(List<Valem> disj){
-        this.disjunktsiooniValemid = disj;
+    public Implikatsioon(Valem valem1, Valem valem2){
+
+        this.vasakLaps = valem1;
+        this.paremLaps = valem2;
+    }
+
+    public Valem getVasakLaps() {
+        return vasakLaps;
+    }
+
+    public Valem getParemLaps() {
+        return paremLaps;
     }
 
     @Override
     public List<Object> getChildren() {
-        return Arrays.asList((Object) disjunktsiooniValemid);
+        return Arrays.asList((Object) vasakLaps, paremLaps);
     }
 
     @Override
     public boolean vaartusta(Map<Character, Double> vaartustus) {
 
-        return  disjunktsiooniValemid.stream()
+        return implikatsioon(vasakLaps.vaartusta(vaartustus), paremLaps.vaartusta(vaartustus));
+
+        /*return  disjunktsiooniValemid.stream()
                 .map(valem -> valem.vaartusta(vaartustus))
-                .reduce((valem1,valem2) -> valem1 || valem2).get();
+                .reduce((valem1,valem2) -> valem1 || valem2).get();*/
+    }
+
+    private boolean implikatsioon(boolean valem1, boolean valem2) {
+        if(valem1 && !valem2){
+            return false;
+        }
+        return true;
     }
 
     @Override
     public Set<IndiviidTerm> getIndiviidTermid() {
         Set<IndiviidTerm> indiviidTermid = new HashSet<>();
 
-        for(Valem valem : disjunktsiooniValemid){
+        indiviidTermid.addAll(paremLaps.getIndiviidTermid());
+        indiviidTermid.addAll(vasakLaps.getIndiviidTermid());
+
+        /*for(Valem valem : disjunktsiooniValemid){
             indiviidTermid.addAll(valem.getIndiviidTermid());
-        }
+        }*/
         return indiviidTermid;
     }
 
     public Set<Character> getVabadMuutujad() {
+
         Set<Character> vabad = new HashSet<>();
-        for(Valem valem : disjunktsiooniValemid){
+
+        vabad.addAll(paremLaps.getVabadMuutujad());
+        vabad.addAll(vasakLaps.getVabadMuutujad());
+
+        /*for(Valem valem : disjunktsiooniValemid){
             vabad.addAll(valem.getVabadMuutujad());
-        }
+        }*/
+
         return vabad;
+    }
+
+
+    public static Valem looImplikatsioonid(List<Valem> alamValemid) {
+
+        Valem vasak = alamValemid.get(0);
+
+        for(int i = 1; i < alamValemid.size(); i++){
+            vasak = new Implikatsioon(vasak, alamValemid.get(i));
+
+        }
+
+        return vasak;
+    }
+
+    public List<TõesuspuuTipp> reegel(boolean tõeväärtus){
+
+        if(tõeväärtus){
+
+            return Arrays.asList(new TõesuspuuTipp(this.vasakLaps, false), new TõesuspuuTipp(this.paremLaps, true));
+        }
+        else{
+            //kui hargnemist ei ole, siis alluv seatakse vasakule
+            TõesuspuuTipp laps = new TõesuspuuTipp(this.vasakLaps, true);
+            TõesuspuuTipp lapseLaps = new TõesuspuuTipp(this.paremLaps, false);
+            laps.setVasakLaps(lapseLaps);
+
+            return Arrays.asList(laps);
+
+        }
     }
 }
