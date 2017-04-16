157344173a16d48f94acf130c2d6208856278dfe
Siiri Saar <siiri.saar1307@gmail.com>
2017-04-16 22:29:42 +0300

    Negation becomes a separate node in the tree

--- src/main/java/predmoodul/kvantorid/Eitus.java
+++ src/main/java/predmoodul/kvantorid/Eitus.java
@@ -1,7 +1,59 @@
 package predmoodul.kvantorid;
 
+import predmoodul.termid.IndiviidTerm;
+import predmoodul.valemid.TõesuspuuTipp;
+import predmoodul.valemid.Valem;
+
+import java.util.*;
+
 /**
  * Created by siiri on 13/04/17.
  */
-public class Eitus {
+public class Eitus extends Valem implements Kvantor {
+
+    private Valem valem;
+
+    public Eitus(Valem valem){
+        this.valem = valem;
+    }
+
+    @Override
+    public Character getIndiviidMuutuja() {
+        return null;
+    }
+
+
+    @Override
+    public boolean vaartusta(Map<Character, Double> vaartustus) {
+
+        return !valem.vaartusta(vaartustus);
+    }
+
+    @Override
+    public Set<IndiviidTerm> getIndiviidTermid() {
+        return null;
+    }
+
+    @Override
+    public Set<Character> getVabadMuutujad() {
+
+        return valem.getVabadMuutujad();
+    }
+
+    @Override
+    public List<Object> getChildren() {
+        return Arrays.asList((Object) valem);
+    }
+
+    public List<TõesuspuuTipp> reegel(boolean tõeväärtus) {
+
+        if(tõeväärtus){
+
+            return Arrays.asList(new TõesuspuuTipp(this.valem, false));
+        }
+        else{
+
+            return Arrays.asList(new TõesuspuuTipp(this.valem, true));
+        }
+    }
 }
