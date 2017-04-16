1b3deaaf2e811aacfbcb6573641081d117449f6d
Siiri Saar <siiri.saar1307@gmail.com>
2017-04-16 22:31:18 +0300

    All quantifier becomes a separate node in the tree

--- src/main/java/predmoodul/kvantorid/Iga.java
+++ src/main/java/predmoodul/kvantorid/Iga.java
@@ -1,14 +1,31 @@
 package predmoodul.kvantorid;
 
+import predmoodul.termid.IndiviidTerm;
+import predmoodul.valemid.TõesuspuuTipp;
+import predmoodul.valemid.Valem;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by siiri on 09/03/17.
  */
-public class Iga implements Kvantor {
+public class Iga extends Valem implements Kvantor {
 
+    private Valem valem;
     private Character indiviidmuutuja;
 
-    public Iga(Character indiviidmuutuja){
+    public Iga(Valem valem, Character indiviidmuutuja){
+        this.valem = valem;
         this.indiviidmuutuja = indiviidmuutuja;
+    }
+
+    @Override
+    public List<Object> getChildren() {
+        return Arrays.asList((Object) valem, indiviidmuutuja);
     }
 
     @Override
@@ -23,8 +40,38 @@ public class Iga implements Kvantor {
         return this.indiviidmuutuja;
     }
 
+
     @Override
-    public boolean lõpetamiseTingimus() {
-        return false;
+    public boolean vaartusta(Map<Character, Double> vaartustus) {
+
+        for(double i = 0; i < 100; i++){
+            vaartustus.put(indiviidmuutuja, i);
+            boolean valemiVaartus = valem.vaartusta(vaartustus);
+            if(!valemiVaartus){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public Set<IndiviidTerm> getIndiviidTermid() {
+        return null;
+    }
+
+    @Override
+    public Set<Character> getVabadMuutujad() {
+
+        Set<Character> valemiVabadMuutujad = valem.getVabadMuutujad();
+        valemiVabadMuutujad.remove(indiviidmuutuja);
+
+        return valemiVabadMuutujad;
+    }
+
+    @Override
+    public List<TõesuspuuTipp> reegel(boolean tõeväärtus) {
+
+        throw new NotImplementedException();
     }
 }
