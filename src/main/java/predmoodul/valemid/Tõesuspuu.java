28a8c96361254f83ff496311faaa7717633133d6
Siiri Saar <siiri.saar1307@gmail.com>
2017-05-01 13:35:24 +0300

    Method for drawing a truthtree with DOT; contradicting node not to be added among the nodes-to-be-processed

--- src/main/java/predmoodul/valemid/Tõesuspuu.java
+++ src/main/java/predmoodul/valemid/Tõesuspuu.java
@@ -1,8 +1,6 @@
 package predmoodul.valemid;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Created by siiri on 13/04/17.
@@ -30,6 +28,10 @@ public class Tõesuspuu {
 
             TõesuspuuTipp tipp = jrk.remove();
 
+            if(tipp.sisaldabVastuolu()){ //kui tipp annab vastuolu, siis ei lisa teda töödeldavate tippude järjekorda
+                continue;
+            }
+
             if(tipp.onAnalüüsitud()){
                 continue;
             }
@@ -48,9 +50,13 @@ public class Tõesuspuu {
 //            }
 
             for (TõesuspuuTipp leht : tipp.getLehed()) {
+                if(leht.sisaldabVastuolu()){
+                    continue;
+                }
                 TõesuspuuTipp vasakLaps = new TõesuspuuTipp(alampuud.get(0));
                 lisaJärjekorda(jrk, vasakLaps);
                 leht.setVasakLaps(vasakLaps);
+
                 if(alampuud.size() == 2){
                     TõesuspuuTipp paremLaps = new TõesuspuuTipp(alampuud.get(1));
                     lisaJärjekorda(jrk, paremLaps);
@@ -71,6 +77,56 @@ public class Tõesuspuu {
 
     public void looPuu() {
         lisaTipp(juurtipp);
+    }
+
+    public Set<Map<String, Boolean>> vaartustusedVastavaltEeldusele(){
+
+        Set<Map<String, Boolean>> vaartustused = new HashSet<>();
+
+        Set<TõesuspuuTipp> lehedPuus = juurtipp.getLehed();
+
+        System.out.println("Selles puus on " + lehedPuus.size() + " lehte.");
+
+        for(TõesuspuuTipp leht : lehedPuus){
+
+            System.out.println("Leidsin lehe: " + leht.toString());
+
+            if(!leht.sisaldabVastuolu()){
+                System.out.println("Leidsin väärtustuse mil valem on väär");
+                vaartustused.add(leht.tagastaVaartustused());
+            }
+        }
+
+        return vaartustused;
+    }
+
+    private String valmistaDotFormaat(TõesuspuuTipp tipp){
+
+        Queue<TõesuspuuTipp> järjekord = new LinkedList<>();
+
+        järjekord.add(tipp);
+
+        StringBuilder sisend = new StringBuilder();
+
+        while(!järjekord.isEmpty()){
+            TõesuspuuTipp puuTipp = järjekord.remove();
+            sisend.append(puuTipp.dotFormaat());
+            if(puuTipp.getVasakLaps() != null){
+                järjekord.add(puuTipp.getVasakLaps());
+            }
+            if(puuTipp.getParemLaps() != null){
+                järjekord.add(puuTipp.getParemLaps());
+            }
+        }
+
+        return sisend.toString();
+    }
+
+    public String dot() {
+        if(this.juurtipp == null){
+            throw new IllegalArgumentException("Tõesuspuul puudub juurtipp!");
+        }
+        return valmistaDotFormaat(this.juurtipp);
     }
 
     @Override
