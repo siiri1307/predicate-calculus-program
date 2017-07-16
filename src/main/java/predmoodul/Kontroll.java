263b732cf788cd12d8d38ed0f9181a1619f98da0
Siiri Saar <siiri.saar1307@gmail.com>
2017-07-16 23:00:14 +0300

    Input is checked for expected number of free variables; added logic for returning the counter model

--- src/main/java/predmoodul/Kontroll.java
+++ src/main/java/predmoodul/Kontroll.java
@@ -1,10 +1,12 @@
 package predmoodul;
 
+import predmoodul.erindid.ErinevIndiviidideArv;
 import predmoodul.kvantorid.Iga;
 import predmoodul.valemid.Ekvivalents;
 import predmoodul.valemid.Valem;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -14,16 +16,69 @@ public class Kontroll {
 
     private Valem pakkumine;
     private Valem vastus;
+    private Valem valem;
+    private Map<Character, Double> vaartustused;
 
     public Kontroll(Valem pakkumine, Valem vastus) {
         this.pakkumine = pakkumine;
         this.vastus = vastus;
+        this.vaartustused = new HashMap<>();
     }
 
-    public boolean eiOleSamavaarne(){
+    public Kontroll(Valem valem){
+
+        this.valem = valem;
+        this.vaartustused = new HashMap<>();
+    }
+
+    public boolean eiOleSamavaarne() throws ErinevIndiviidideArv {
+        int indiviidideArvPakkumine = pakkumine.getVabadMuutujad().size();
+        //System.out.println("Pakkumine: " + pakkumine.getVabadMuutujad());
+        int indiviidideArvVastus = vastus.getVabadMuutujad().size();
+        //System.out.println("Vastus: " + vastus.getVabadMuutujad());
+        if(indiviidideArvPakkumine != indiviidideArvVastus){
+            throw new ErinevIndiviidideArv(indiviidideArvPakkumine, indiviidideArvVastus);
+        }
         Valem ekvivalentsiValem = moodustaEkvivalentsiValem(pakkumine, vastus);
         Valem valemVabadeMuutujateta = seoVabadMuutujad(ekvivalentsiValem);
-        return !valemVabadeMuutujateta.vaartusta(new HashMap<>());
+        //System.out.println("Valem vabade muutujateta: " + valemVabadeMuutujateta.getVabadMuutujad());
+        return !valemVabadeMuutujateta.vaartusta(vaartustused);
+    }
+
+    public boolean eiOleSamavaarne2(){
+        Valem valemVabadeMuutujateta = seoVabadMuutujad(valem);
+        return !valemVabadeMuutujateta.vaartusta(vaartustused);
+    }
+
+    public Map<Character, Integer> tagastaKontramudel(){
+
+        Map<Character, Integer> kontramudel = new HashMap<>();
+
+        Set<Character> vabadMuutujad = pakkumine.getVabadMuutujad();
+
+        //System.out.println("Vabu muutujaid on: " + vabadMuutujad.size());
+
+        for(Character ch : vabadMuutujad){
+
+            if(vaartustused.containsKey(ch)){
+                kontramudel.put(ch, vaartustused.get(ch).intValue());
+            }
+        }
+
+        return kontramudel;
+    }
+
+    public String kontraNaideStringina(){
+
+        Map<Character, Integer> kontramudel = tagastaKontramudel();
+
+        //boolean tudengiVastuseToevaartus = ((Ekvivalents) valem).getVasakLaps().vaartusta(vaartustused); //eeldus: tudengi pakkumine on ekvivalentsi vasak pool
+        //boolean oigeVastuseToevaartus = ((Ekvivalents) valem).getParemLaps().vaartusta(vaartustused);
+        boolean tudengiVastuseToevaartus = pakkumine.vaartusta(vaartustused);
+        boolean oigeVastuseToevaartus = vastus.vaartusta(vaartustused);
+        String infoTudengile = "Sinu vastus on väärtustusel " + kontramudel + " " + tudengiVastuseToevaartus + ", aga peaks olema " + oigeVastuseToevaartus;
+
+        return infoTudengile;
     }
 
     private Valem seoVabadMuutujad(Valem ekvivalentsiValem) {
