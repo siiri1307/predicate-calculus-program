407d7795f299547148ebba51a6f74f3fb60823ad
Siiri Saar <siiri.saar1307@gmail.com>
2017-07-16 21:58:12 +0300

    Custom exception to be thrown when input contains unanticipated number of free variables

--- src/main/java/predmoodul/erindid/ErinevIndiviidideArv.java
+++ src/main/java/predmoodul/erindid/ErinevIndiviidideArv.java
@@ -3,5 +3,16 @@ package predmoodul.erindid;
 /**
  * Created by siiri on 28/06/17.
  */
-public class ErinevIndiviidideArv {
+public class ErinevIndiviidideArv extends Throwable {
+
+    private String sonum;
+
+    public ErinevIndiviidideArv(int indiviidideArvPakkumine, int indiviidideArvLahendus){
+        this.sonum = "Esitasid " + indiviidideArvPakkumine + "-kohalise predikaadi, ootasin aga " + indiviidideArvLahendus + "-kohalist predikaati.";
+    }
+
+    @Override
+    public String getMessage(){
+        return sonum;
+    }
 }
