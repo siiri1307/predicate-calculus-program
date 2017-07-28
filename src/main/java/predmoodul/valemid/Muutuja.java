27bbb22f03b744e30f5f52e3dfce3e6a7b38a238
Siiri Saar <siiri.saar1307@gmail.com>
2017-07-28 22:34:20 +0300

    Add class Muutuja to denote an argument

--- src/main/java/predmoodul/valemid/Muutuja.java
+++ src/main/java/predmoodul/valemid/Muutuja.java
@@ -10,6 +10,15 @@ public class Muutuja extends AstNode {
 
     private Character tahis;
     private int jarjeNumber;
+    private String predTahis;
+
+    public String getPredTahis() {
+        return predTahis;
+    }
+
+    public void setPredTahis(String predTahis) {
+        this.predTahis = predTahis;
+    }
 
     public Muutuja(Character tahis){
         this.tahis = tahis;
@@ -18,6 +27,11 @@ public class Muutuja extends AstNode {
     public Muutuja(Character tahis, int nr){
         this.tahis = tahis;
         this.jarjeNumber = nr;
+    }
+
+    public Muutuja(Character tahis, String predTahis) {
+        this.tahis = tahis;
+        this.predTahis = predTahis;
     }
 
     public Character getTahis() {
@@ -30,8 +44,51 @@ public class Muutuja extends AstNode {
         return jarjeNumber;
     }
 
+    public void setTahis(Character tahis) {
+        this.tahis = tahis;
+    }
+
+    public void setJarjeNumber(int jarjeNumber) {
+        this.jarjeNumber = jarjeNumber;
+    }
+
+    @Override
+    public boolean equals(Object muutuja){
+
+        if(this == muutuja){
+            return true;
+        }
+        else if(muutuja == null || this.getClass() != muutuja.getClass()){
+            return false;
+        }
+
+        Muutuja m = (Muutuja) muutuja;
+
+        return this.tahis.equals(m.getTahis()) && this.jarjeNumber == m.getJarjeNumber() && samaAbiValem(m.getPredTahis());
+
+    }
+
+    private boolean samaAbiValem(String teiseMuutujaPredTahis) {
+        if(this.predTahis == null){
+            return teiseMuutujaPredTahis == null;
+        }
+        return this.predTahis.equals(teiseMuutujaPredTahis);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = tahis.hashCode();
+        result = 31 * result + jarjeNumber;
+        return result;
+    }
+
     @Override
     public List<Object> getChildren() {
         return Arrays.asList((Object) tahis);
+    }
+
+    @Override
+    public String toString() {
+        return tahis + Integer.toString(jarjeNumber);
     }
 }
